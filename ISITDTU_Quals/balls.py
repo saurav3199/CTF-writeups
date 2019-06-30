c3c1d7516c107c31893672f5fe2195b7c92a65e2
Saurav Kumar Singh <saurav.co.in.com@gmail.com>
2019-06-30 19:35:29 +0530

    Update balls.py

--- ISITDTU_Quals/balls.py
+++ ISITDTU_Quals/balls.py
@@ -4,26 +4,27 @@ import sys
 outs=['The left is lighter than the right','Both are equally heavy','The left is heavier than the right']
 r = remote('34.68.81.63',6666)
 fake='1 2'
-count=0
 
 def res(num):
 	print(num)
 	print(r.recvline())
 	r.sendline(str(num))
 	print('result->' + r.recvline())
+ 
+ 
 while(1):
 	try:
 		s = r.recvline()
 	except EOFError:
 		r.interactive()
 	print(s)
-	count+=1
 	if 'Round' in s:
 		print(r.recvuntil('Weighting 1: '))
 		r.sendline('1,2,3,4 5,6,7,8')
 		output = r.recvline()
 		print(output)
 		print(r.recvuntil('Weighting 2: '))
+  
 		if outs[0] in output:
 			r.sendline('5,6,1 7,2,9')
 			output2 = r.recvline()
@@ -129,4 +130,3 @@ while(1):
 					res(2)
 				if outs[2] in output3:
 					res(1)
-
