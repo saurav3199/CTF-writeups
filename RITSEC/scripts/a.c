f3f7376cf9f480d0ed362e3bf861d2a7be247b9b
Saurav Kumar Singh <saurav.co.in.com@gmail.com>
2019-11-18 21:56:24 +0530

    Update a.c

--- RITSEC/scripts/a.c
+++ RITSEC/scripts/a.c
@@ -5,26 +5,20 @@
 
 int main(int argc, char **argv)
 {
-    unsigned int seq[] = {atoi(argv[1]), atoi(argv[2]), atoi(argv[3]),\
-        atoi(argv[4]), atoi(argv[5])};
-    unsigned int r;
+    unsigned int numbers[] = {atoi(argv[1]), atoi(argv[2]), atoi(argv[3]),atoi(argv[4]), atoi(argv[5])};
+    unsigned int r,t=time(0),j=4;
     for(int i = 0; i < 1000000; i++)
     {
-        srand(time(0) - i);
-        for(int j = 0; j < 5; j++)
-        {    
-            r = rand();
-            if(r != seq[j])
-            {
-                break;
-            }
-            if(j == 4)
-            {
-                printf("%d %d\n", i,rand());
-                exit(0);
-            }
+        srand(t - i);
+        r=rand();
+        if(r!=numbers[0])
+            continue;
+        while(j-->0)
+        {
+            rand();
         }
+        printf("%d\n",rand());
+        break;
     }
-    exit(1);
     return 0;
 }
