7b3e87911babf56fc24f1649d0e41733157198b6
Saurav Kumar Singh <saurav.co.in.com@gmail.com>
2019-11-18 11:21:59 +0530

    Update obtain_stars.py

--- RITSEC/scripts/obtain_stars.py
+++ RITSEC/scripts/obtain_stars.py
@@ -1,8 +1,6 @@
 from math import *
 
-
-stars=open("STAR_TABLES.agc").read()
-stars=stars.split("\n")
+stars=open("STAR_TABLES.agc").read().split("\n")
 dis=[[0 for i in range(3)] for j in range(40)] 
 for i in stars:
     s=i.split(" ")
@@ -10,19 +8,14 @@ for i in stars:
     pos=int(s[1])
     dis[pos][ord(s[2])-ord('X')]=val
 
-
-
 def calc_distance(f,s):
     return sqrt((dis[f][0]-dis[s][0])**2+(dis[f][1]-dis[s][1])**2+(dis[f][2]-dis[s][2])**2)
 
-
 flag=''
-d=open('distances.txt').read()
-d=d.split('\n')
+d=open('distances.txt').read().split('\n')
 for i in d:
     axes=i.split(' ')
-    first=int(axes[1])
-    second=int(axes[-1])
+    first,second=int(axes[1]),int(axes[-1])
     flag+=chr(int(calc_distance(first,second)*100))
 
 print(flag)
