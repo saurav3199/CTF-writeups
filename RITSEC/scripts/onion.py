4be3c651c3ee6fa5177fb687c06ce82369752a1f
Saurav Kumar Singh <saurav.co.in.com@gmail.com>
2019-11-18 11:51:52 +0530

    Update onion.py

--- RITSEC/scripts/onion.py
+++ RITSEC/scripts/onion.py
@@ -1,6 +1,5 @@
 import base64
 flag=open("onionlayerencoding.txt","r").read()
-run=0
 while "RITSEC" not in str(flag):
     try:
         flag=base64.b16decode(flag)
@@ -9,7 +8,5 @@ while "RITSEC" not in str(flag):
             flag=base64.b32decode(flag)
         except:
             flag=base64.b64decode(flag)
-    print(run)
-    run+=1
 
 print(flag)
