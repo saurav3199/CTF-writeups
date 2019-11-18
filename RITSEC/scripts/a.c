cfe74edb30d6fa4317e53f38714ea1827cfdf6fc
Saurav Kumar Singh <saurav.co.in.com@gmail.com>
2019-11-18 22:07:07 +0530

    Update a.c

--- RITSEC/scripts/a.c
+++ RITSEC/scripts/a.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
 {
     unsigned int numbers[] = {atoi(argv[1]), atoi(argv[2]), atoi(argv[3]),atoi(argv[4]), atoi(argv[5])};
     unsigned int r,t=time(0),j=4;
-    for(int i = 0; i < 1000000; i++)
+    for(int i = 0; i < 2; i++)
     {
         srand(t - i);
         r=rand();
